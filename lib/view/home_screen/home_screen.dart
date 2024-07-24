45becfd421181dde7823fb575ef0fa3dbe8c32d7
shihabLuminar <neethu@luminartechnolab.com>
2024-07-24 15:20:51 +0530

    home page development

--- lib/view/home_screen/home_screen.dart
+++ lib/view/home_screen/home_screen.dart
@@ -1,10 +1,36 @@
 import 'package:flutter/material.dart';
+import 'package:netflix_may/utils/color_constants.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      backgroundColor: ColorConstants.mainBlack,
+      body: Stack(
+        children: [
+          Container(
+            decoration: BoxDecoration(
+                image: DecorationImage(
+                    fit: BoxFit.cover,
+                    image: NetworkImage(
+                        "https://i.pinimg.com/564x/b1/8e/08/b18e0850f61301cce40519c584c7b1ba.jpg"))),
+            height: 415,
+          ),
+          Container(
+            decoration: BoxDecoration(
+                gradient: LinearGradient(
+                    begin: Alignment.bottomCenter,
+                    end: Alignment.topCenter,
+                    colors: [
+                  ColorConstants.mainBlack,
+                  Colors.transparent,
+                ])),
+            height: 415,
+          ),
+        ],
+      ),
+    );
   }
 }
