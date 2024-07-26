cf41432b060d324bfe0d88f8210b6fe4ebd73574
shihabLuminar <neethu@luminartechnolab.com>
2024-07-26 15:12:49 +0530

    hoem screen top ten section

--- lib/utils/image_constants.dart
+++ lib/utils/image_constants.dart
@@ -7,4 +7,5 @@ class ImageConstants {
   static const String USER3 = "assets/images/user_3.png";
   static const String USER4 = "assets/images/user_4.png";
   static const String ADD_BUTTON = "assets/images/add_png.png";
+  static const String LOGO_N = "assets/images/logos_N.png";
 }
--- lib/view/home_screen/home_screen.dart
+++ lib/view/home_screen/home_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:netflix_may/utils/color_constants.dart';
+import 'package:netflix_may/utils/image_constants.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
@@ -8,29 +9,83 @@ class HomeScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: ColorConstants.mainBlack,
-      body: Stack(
+      body: Column(
         children: [
-          Container(
-            decoration: BoxDecoration(
-                image: DecorationImage(
-                    fit: BoxFit.cover,
-                    image: NetworkImage(
-                        "https://i.pinimg.com/564x/b1/8e/08/b18e0850f61301cce40519c584c7b1ba.jpg"))),
-            height: 415,
-          ),
-          Container(
-            decoration: BoxDecoration(
-                gradient: LinearGradient(
-                    begin: Alignment.bottomCenter,
-                    end: Alignment.topCenter,
-                    colors: [
-                  ColorConstants.mainBlack,
-                  Colors.transparent,
-                ])),
-            height: 415,
-          ),
+          // #1 top ten section
+          _buildTopTenSection(),
+          SizedBox(height: 20),
+          //#2 play button section
+          Row(
+            children: [Column()],
+          )
         ],
       ),
+    );
+  }
+
+  Widget _buildTopTenSection() {
+    return Stack(
+      children: [
+        Container(
+          decoration: BoxDecoration(
+              image: DecorationImage(
+                  fit: BoxFit.cover,
+                  image: NetworkImage(
+                      "https://i.pinimg.com/564x/b1/8e/08/b18e0850f61301cce40519c584c7b1ba.jpg"))),
+          height: 415,
+        ),
+        Container(
+          padding: EdgeInsets.only(left: 10, right: 24, top: 45),
+          decoration: BoxDecoration(
+              gradient: LinearGradient(
+                  begin: Alignment.bottomCenter,
+                  end: Alignment.topCenter,
+                  colors: [
+                ColorConstants.mainBlack,
+                Colors.transparent,
+              ])),
+          height: 415,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Image.asset(ImageConstants.LOGO_N),
+                  Text(
+                    "Tv Shows",
+                    style: TextStyle(
+                      color: ColorConstants.mainWhite,
+                      fontSize: 17.2,
+                    ),
+                  ),
+                  Text(
+                    "Movies",
+                    style: TextStyle(
+                      color: ColorConstants.mainWhite,
+                      fontSize: 17.2,
+                    ),
+                  ),
+                  Text(
+                    "My List",
+                    style: TextStyle(
+                      color: ColorConstants.mainWhite,
+                      fontSize: 17.2,
+                    ),
+                  ),
+                ],
+              ),
+              Text(
+                "#2 in Nigeria Today",
+                style: TextStyle(
+                    color: ColorConstants.mainWhite,
+                    fontSize: 14,
+                    fontWeight: FontWeight.bold),
+              ),
+            ],
+          ),
+        ),
+      ],
     );
   }
 }
